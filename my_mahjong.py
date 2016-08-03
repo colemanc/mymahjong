fb5c039e379f1b6b08c60f2b29e3d7ccc4ddc30f
colemanc <colemanc@cox.net>
2016-08-02 21:15:56 -0400

    maoving the menu code into a class

--- my_mahjong.py
+++ my_mahjong.py
@@ -1,15 +1,39 @@
-import tkinter as tk
+#import tkinter as tk
 from tkinter import *
 from tkinter.font import Font, nametofont
 from mahjmath import *
 
+
+class Menubar(Menu):
+    def __init__(self, parent):
+        Menu.__init__(self, parent)
+        gamemenu = Menu(self)
+        self.add_cascade(label="Game", menu=gamemenu)
+        gamemenu.add_command(label="Play Game", command=self.playGame)
+        gamemenu.add_command(label="Game Statistics", command=self.openStatistics)
+        gamemenu.add_separator()
+        gamemenu.add_command(label="Exit", command=self.quit)
+
+        helpmenu = Menu(self)
+        self.add_cascade(label="Help", menu=helpmenu)
+        helpmenu.add_command(label="About...", command=self.about)
+
+    def quit(self):
+       sys.exit(0)
+
+
+    def playGame(): pass
+
+    def about(self):
+        print("This is a simple mahjong game that keeps track of board numbers.")
+
+    def openStatistics(): pass
+
+
 class Application(Frame):
 
-    def __init__(self, master=None):
-        root = Tk()
-
-        #root.tk.call('tk', 'scaling', 20.0)
-        Frame.__init__(self, master)
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
         self.pack
         self.grid()
         #screen_height = root.winfo_screenheight()
@@ -19,27 +43,12 @@ class Application(Frame):
         #myfont.configure(size=36)
         #print(screen_height)
         self.create_board(1)
+        menubar = Menubar(parent)
+        parent.config(menu=menubar)
+                #creeate menu
 
-        #creeate menu
-        menu = Menu(root)
-        root.config(menu=menu)
-        gamemenu = Menu(menu)
-        menu.add_cascade(label="Game", menu=gamemenu)
-        gamemenu.add_command(label="Play Game", command=self.playGame)
-        gamemenu.add_command(label="Game Statistics", command=self.openStatistics)
-        gamemenu.add_separator()
-        gamemenu.add_command(label="Exit", command=root.quit)
 
-        helpmenu = Menu(menu)
-        menu.add_cascade(label="Help", menu=helpmenu)
-        helpmenu.add_command(label="About...", command=self.about)
 
-    def playGame(): pass
-
-    def about(self):
-        print("This is a simple mahjong game that keeps track of board numbers.")
-
-    def openStatistics(): pass
 
     def fixfonts(self):
         #screen_width = self.winfo_screenwidth()
@@ -55,6 +64,10 @@ class Application(Frame):
         self.a_label = Label(self, text = "what")
         self.a_label.grid(row = 0, column = 1)
 
-app = Application()
+
+
+root = Tk()
+app = Application(parent=root)
 
 app.mainloop()
+root.destroy()
